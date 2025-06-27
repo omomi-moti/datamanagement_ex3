d96f9d5687fe7de6053a8ee694c0bf97e2db1c35
omomi-moti <ikpubon@icloud.com>
2025-06-27 11:34:50 +0900

    Update daice_game.java
    Won

--- daice_game.java
+++ daice_game.java
@@ -8,8 +8,7 @@ public class DiceGame {
     public static void main(String[] args) {
         // Randomクラスのインスタンスを生成して、乱数を扱えるようにします。
         Random randomGenerator = new Random();
-　　　　 System.out.println("What iss your name ?");
-        System.out.println("Hello John")
+　　　　 
         System.out.println("Rolling dice..."); // 日本語: サイコロを振っています...
 
         // 1から6までの乱数を生成します (nextInt(6)は0-5の範囲なので+1します)。
@@ -23,5 +22,12 @@ public class DiceGame {
         System.out.println("Die 1: " + die1); // 日本語: サイコロ1
         System.out.println("Die 2: " + die2); // 日本語: サイコロ2
         System.out.println("Total value: " + totalValue); // 日本語: 合計値
+        if(totalValue>8){
+            System.out.print("You won");
+        }
+        else{
+        System.out.println("You lost");
+        } 
+        
     }
 }
